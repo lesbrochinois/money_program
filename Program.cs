36d32e81d7fa56db974ace083743c2385bb7955b
Theophile Defeche <theophiledefeche@ik.me>
2024-12-25 00:58:04 +0100

    make the base template

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+  // MARK: Main
+  static void Main(string[] args)
+  {
+    Random rnd = new();
+    int product_price = rnd.Next(1, 60);
+    Console.WriteLine($"The product costs {product_price}$");
+    Console.Write("Enter your money : ");
+    int user_money = int.Parse(Console.ReadLine() ?? "");
+
+    if(user_money > product_price)
+    {
+      // Give money back
+      Console.WriteLine("Give money back");
+    }
+    else if(user_money == product_price)
+    {
+      // Perfect
+      Console.WriteLine("Perfect");
+    }
+    else
+    {
+      // You need money
+      Console.WriteLine("You need money");
+    }
+
+  }
+}
